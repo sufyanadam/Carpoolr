1c439983ad290cf75ae613a81cecdcfd2cb131e9
Majik Bytes <Majik.Bytes@majikbytes.com>
2011-11-16 20:10:24 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carpoolr::Application.config.secret_token = '2afefe855dc7605b76937dfbc82a669ec6905f96d767c084eaa5cd1ed1c00876f986b70bc232774d82e787d2f536fb288e14cbf5125482cfb53afb54aa564567'
