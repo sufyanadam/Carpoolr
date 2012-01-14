f865b8590cf5ccb893e901875649f350ece728e0
sufyanadam <sufyan.adam@gmail.com>
2012-01-13 23:11:32 -0800

    Changed migration scale and precision

--- db/migrate/20111117044247_initial.rb
+++ db/migrate/20111117044247_initial.rb
@@ -2,8 +2,8 @@ class Initial < ActiveRecord::Migration
   def change
     create_table :drivers do |t|
       t.integer :number_of_seats, :null => false, :default => 0
-      t.decimal :lat, :precision => 3, :scale => 10
-      t.decimal :lng, :precision => 3, :scale => 10
+      t.decimal :lat, :precision => 10, :scale => 5
+      t.decimal :lng, :precision => 10, :scale => 5
       t.timestamp :last_seen_at
       t.timestamps
     end
@@ -16,8 +16,8 @@ class Initial < ActiveRecord::Migration
 
     create_table :pickup_spots do |t|
       t.string :name
-      t.decimal :lat, :precision => 3, :scale => 10
-      t.decimal :lng, :precision => 3, :scale => 10
+      t.decimal :lat, :precision => 10, :scale =>5
+      t.decimal :lng, :precision => 10, :scale => 5
       t.timestamps
     end
   end
