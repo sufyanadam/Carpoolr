5c2f43d5c72074255c578747caee2139a50587af
sufyanadam <sufyan.adam@gmail.com>
2012-01-29 21:28:50 -0800

    fixed registration issue for destination spots

--- app/models/destination_spot.rb
+++ app/models/destination_spot.rb
@@ -1,6 +1,6 @@
 class DestinationSpot < ActiveRecord::Base
   attr_accessible :name
-  has_many :waiting_riders, :conditions => Rider.waiting_condition, :class_name => "Rider"
-  has_many :waiting_drivers, :conditions => Driver.waiting_condition, :class_name => "Driver"
+  has_many :waiting_riders, :conditions => lambda { |_| Rider.waiting_condition }, :class_name => "Rider"
+  has_many :waiting_drivers, :conditions => lambda { |_| Driver.waiting_condition }, :class_name => "Driver"
   
 end
