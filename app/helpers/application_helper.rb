1a375393e3b61ce95fa2c33121ea412afbca233a
Sufyan Adam <Sufyan@Sufyans-MacBook-Air.local>
2012-04-11 20:17:04 -0700

    fix errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def button_link_to(content, url, options = {})
-    link_to(content, url, options.reverse_merge("data-role" => "button"))
+    link_to(content, url, options.reverse_merge("data-role" => "button", "data-ajax"=>"false" ))
   end
 end
